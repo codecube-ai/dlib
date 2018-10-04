2ad82a6beb037cf6d27fae7b244cc5ca27060f6a
Symond Wang <wzc5840@gmail.com>
2018-10-04 19:15:37 +0900

    Temp commit

--- app/demo.cpp
+++ app/demo.cpp
@@ -1,5 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <iostream>
+#include <fstream>
+
 #include "cc_common.h"
 
+using namespace std;
 
 int main(int argc, char * argv[])
 {
